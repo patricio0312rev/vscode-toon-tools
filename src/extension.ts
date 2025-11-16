f469a6b385a29e2aeb0e0fc15398975f99b46b42
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2025-11-16 14:38:59 -0500

    refactor: register new commands on extension file

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,21 @@ import * as vscode from "vscode";
 import { registerConvertCommands } from "./commands/convert";
 import { registerFileCommands } from "./commands/convertToFile";
 import { registerMinifyCommand } from "./commands/minify";
+import { registerPrettifyCommand } from "./commands/prettify";
 import { registerPreviewCommand } from "./preview/preview";
 import { registerTableViewerCommand } from "./table/tableViewer";
 import { registerAnalyzerCommand } from "./analyzer/sizeAnalyzer";
+import { registerStatusBar } from "./statusBar/statusBar";
 
 export function activate(context: vscode.ExtensionContext) {
   registerConvertCommands(context);
   registerFileCommands(context);
   registerMinifyCommand(context);
+  registerPrettifyCommand(context);
   registerPreviewCommand(context);
   registerTableViewerCommand(context);
   registerAnalyzerCommand(context);
+  registerStatusBar(context);
 }
 
 export function deactivate() {}
